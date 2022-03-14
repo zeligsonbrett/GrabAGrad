510aab2c0695180600edcffaf469a8b72f7f58e9
Henry Knoll <hknoll@princeton.edu>
2022-03-14 18:29:12 -0400

    Connected to SQL database successfully

--- app/model/database_connection.py
+++ app/model/database_connection.py
@@ -2,8 +2,7 @@
 
 from dotenv import load_dotenv
 import os
-from sqlalchemy import create_engine
-
+import sqlalchemy as sqla
 
 def get_uri():
     """
@@ -17,11 +16,59 @@ def get_uri():
         uri = uri.replace("postgres://", "postgresql://", 1)
     return uri
 
-
-def connect_engine():
+def create_engine():
     """
     Creates an engine linked to the database
     :return: sqlalchemy engine linked to postgresql database
     """
-    return create_engine(get_uri())
+    return sqla.create_engine(get_uri())
+
+def query_all_from_table(engine, table):
+    """
+    Queries all rows from the table given by parameter table and prints
+    each row out
+    """
+    with engine.connect() as con:
+        output = con.execute('SELECT * FROM {}'.format(table))
+        print(output.keys())
+        for row in output:
+            print(row)
+
+def query_all_grads(engine):
+    """
+    Prints all rows in the 'graduates' table
+    """
+    query_all_from_table(engine, 'graduates')
+
+def add_a_grad(engine):
+    """
+    Adds a grad to the 'graduates' table - BETA VERSION
+    *Need to add grad information to other tables too*
+    """
+    with engine.connect() as con:
+        graduates_info = ({ "id":           11, 
+                            "name":         "Jane Doe SPA", 
+                            "acad_dept":    "Spanish", 
+                            "bio":          "DELETETHISLATER", 
+                            "photo_link":   "later", 
+                            "website_link": "later"})
+        statement = sqla.text("""INSERT INTO graduates(id, name, acad_dept, bio, photo_link, website_link) VALUES(:id, :name, :acad_dept, :bio, :photo_link, :website_link)""")
+        output = con.execute(statement, **new)
+        print(output)
+
+def del_a_grad(engine):
+    """
+    Deletes a grad from the 'graduates' table by ID
+    """
+    with engine.connect() as con:
+        output = con.execute('DELETE FROM graduates WHERE id == 11')
+        print(output)
+
+
+if __name__ == '__main__':
+    engine = create_engine()
+    #add_a_grad(engine)
+
+    # Different tables so far: ['graduates', 'grad_industries', 'grad_experiences', 'grad_interests', 'grad_contact']
+    query_all_grads(engine)
 
