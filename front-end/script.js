e40b2a871bf9d0d3a31c23340b3d3990385616df
zeligsongit <zeligson@princeton.edu>
2022-03-14 23:48:51 -0400

    front-end

---
+++ front-end/script.js
@@ -0,0 +1,14 @@
+function focusChange() {
+    const topNav = document.getElementById("card");
+    if (topNav.className.includes("focus")) {
+      topNav.classList.remove("focus");
+    } else {
+      topNav.classList.add("focus");
+  
+  
+  
+  
+  
+   
+  
+  
