4d1375f49157222c1c9d7a6bd4111ef75a6de6a4
Henry Knoll <hknoll@princeton.edu>
2022-03-19 17:57:19 -0400

    Back-end updates

--- app/model/database_connection.py
+++ app/model/database_connection.py
@@ -4,6 +4,7 @@ from dotenv import load_dotenv
 import os
 import sqlalchemy as sqla
 
+engine = None
 
 def get_uri():
     """
@@ -23,8 +24,39 @@ def create_engine():
     Creates an engine linked to the database
     :return: sqlalchemy engine linked to postgresql database
     """
-    print(get_uri())
-    return sqla.create_engine(get_uri())
+    global engine
+    engine = sqla.create_engine(get_uri())
 
-#     Different tables so far: ['graduates', 'grad_industries',
-#     'grad_experiences', 'grad_interests', 'grad_contact']
+def query_all_from_table(table):
+    """
+    Queries all rows from the table given by parameter table and prints
+    each row out
+    """
+    with engine.connect() as con:
+        output = con.execute('SELECT * FROM {}'.format(table))
+        print(output.keys())
+        for row in output:
+            print(row)
+
+def execute_command(command, params=None):
+    output = None
+    with engine.connect() as con:
+        if params:
+            output = con.execute(command, params)
+        else:
+            ouput = con.execute(command)
+        return output
+
+def get_last_id_graduates():
+    with engine.connect() as con:
+        output = con.execute('SELECT id FROM graduates')
+        rows = [x for x in output]
+        return rows[-1][0]
+
+def del_id_from_tables(tables, del_id):
+    with engine.connect() as con:
+        for table in tables:
+            statement = 'DELETE FROM ' + table + ' WHERE id = ' + str(del_id)
+            con.execute(statement)
+
+engine = sqla.create_engine(get_uri())
