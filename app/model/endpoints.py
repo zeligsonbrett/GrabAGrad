8239b4f0f2b10a8506e74205be0524a0abd457ac
Henry Knoll <hknoll@princeton.edu>
2022-03-19 17:58:03 -0400

    Back-end updates

--- app/model/endpoints.py
+++ app/model/endpoints.py
@@ -2,49 +2,108 @@
 
 import pandas as pd
 import sqlalchemy as sqla
+import database_connection as db
 
+# Queries the database and returns a dataframe of the results
+def query_all_grads(engine):
+    print("This is running")
+    query_text = 'SELECT * FROM GRADUATES'
+    #with engine.connect() as con:
+        #result = con.execute(query_text)
 
-def query_all_from_table(engine, table):
-    """
-    Queries all rows from the table given by parameter table and prints
-    each row out
-    :return: all rows from the table
-    """
-
-    query_text = 'SELECT * FROM {}'.format(table)
-    output = pd.read_sql(query_text, engine)
-    df = pd.DataFrame(output)
-    result = df.to_string(index=False)
-    print(result)
-
-    # alternative way to do this:
-    # with engine.connect() as con:
-    #     output = con.execute(query_text)
+    # result = pd.read_sql(query_text, engine)
+    # print(result)
+    result = 'dang'
     return result
 
+def query_all_grads():
+    """
+    Prints all rows in the 'graduates' table
+    """
+    db.query_all_from_table('graduates')
 
-def add_a_grad(engine):
-    """
-    Adds a grad to the 'graduates' table - BETA VERSION
-    *Need to add grad information to other tables too*
-    """
-    with engine.connect() as con:
-        graduates_info = ({"id": 11,
-                           "name": "Jane Doe SPA",
-                           "acad_dept": "Spanish",
-                           "bio": "DELETETHISLATER",
-                           "photo_link": "later",
-                           "website_link": "later"})
-        statement = sqla.text(
-            """INSERT INTO graduates(id, name, acad_dept, bio, photo_link, website_link) VALUES(:id, :name, :acad_dept, :bio, :photo_link, :website_link)""")
-        output = con.execute(statement, **new)
-        print(output)
+def __prepare_argument(user_input):
+    prepped_arg = '%' + user_input + '%'
+    return prepped_arg
 
+def search_grads(name='', dept='', bio='', experience='', industry='', interest=''):
+    """
+    Search all grads for matches based on search criteria
+    """
+    name = __prepare_argument(name)
+    dept = __prepare_argument(dept)
+    bio = __prepare_argument(bio)
+    experience = __prepare_argument(experience)
+    industry = __prepare_argument(industry)
+    interest = __prepare_argument(interest)
 
-def del_a_grad(engine):
+    command = sqla.text('SELECT DISTINCT graduates.id, graduates.name FROM graduates, grad_experiences, grad_industries, grad_interests '\
+            ' WHERE graduates.id = grad_industries.id AND graduates.id = grad_experiences.id AND ' \
+            'graduates.id = grad_interests.id AND graduates.name LIKE :name AND graduates.acad_dept LIKE :dept '\
+            'AND graduates.bio LIKE :bio AND grad_experiences.experience LIKE :experience AND grad_industries.industry LIKE :industry AND '\
+            'grad_interests.interest LIKE :interest;')
+    params = {'name': name, 'dept': dept, 'bio': bio, 'experience': experience, 'industry': industry, 'interest': interest}
+    output = db.execute_command(command, params)
+    matching_ids = [x[0] for x in output]
+    return matching_ids
+
+def get_grad_information(idnum):
     """
-    Deletes a grad from the 'graduates' table by ID
+    Gets overview information for a certain id number graduate
     """
-    with engine.connect() as con:
-        output = con.execute('DELETE FROM graduates WHERE id == 11')
-        print(output)
+    command = sqla.text('SELECT * FROM graduates WHERE graduates.id = :id')
+    params = {'id': idnum}
+    output = db.execute_command(command, params)
+    return [x for x in output][0]
+
+def add_a_grad(name=None, dept=None, bio=None, photo_link=None, website_link=None, experiences=None, industries=None, interests=None, email=None, phone=None):
+    """
+    Adds a grad to the database in each respective table based on data inputted
+    """
+    new_id = db.get_last_id_graduates() + 1
+    graduates_info = { "id":          new_id, 
+                      "name":         name, 
+                      "acad_dept":    dept, 
+                      "bio":          bio, 
+                      "photo_link":   photo_link, 
+                      "website_link": website_link}
+    statement = sqla.text("""INSERT INTO graduates(id, name, acad_dept, bio, photo_link, website_link) VALUES(:id, :name, :acad_dept, :bio, :photo_link, :website_link)""")
+    output = db.execute_command(statement, graduates_info)
+
+    if experiences is not None:
+        for experience, desc in experiences:
+            experience_info = {'id': new_id, 'experience': experience, 'experience_desc': desc}
+            statement = sqla.text("""INSERT INTO grad_experiences(id, experience, experience_desc) VALUES(:id, :experience, :experience_desc)""")
+            output = db.execute_command(statement, experience_info)
+
+    if industries is not None:
+        for industry in industries:
+            industry_info = {'id': new_id, 'industry': industry}
+            statement = sqla.text("""INSERT INTO grad_industries(id, industry) VALUES(:id, :industry)""")
+            output = db.execute_command(statement, industry_info)
+
+    if interests is not None:
+        for interest in interests:
+            interest_info = {'id': new_id, 'interest': interest}
+            statement = sqla.text("""INSERT INTO grad_interests(id, interest) VALUES(:id, :interest)""")
+            output = db.execute_command(statement, interest_info)
+
+    if email is not None or phone is not None:
+        contact_info = {'id': new_id, 'email': email, 'phone': phone}
+        statement = sqla.text("""INSERT INTO grad_contact(id, email, phone) VALUES(:id, :email, :phone)""")
+        output = db.execute_command(statement, contact_info)
+
+def del_a_grad(del_id):
+    """
+    Deletes all rows corresponding to id from the graduate tables
+    """
+    #param = {'id': id}
+    #statement = 'DELETE graduates, grad_contact, grad_experiences, grad_industries, grad_interests FROM graduates INNER JOIN grad_contact INNER JOIN grad_experiences INNER JOIN grad_industries INNER JOIN grad_interests ON graduates.id = :id AND grad_contact.id = :id AND grad_experiences = :id AND grad_industries = :id AND grad_interests = :id'
+    #output = db.execute_command(statement, param)
+    db.del_id_from_tables(['grad_contact', 'grad_experiences', 'grad_industries', 'grad_interests', 'graduates'], del_id)
+
+if __name__ == "__main__":
+    #search_grads(name='John')
+    #add_a_grad(name = "Henry", dept = "COS", experiences = [("Software Engineering Intern", "Cellanome")], interests = ['Football', 'Basketball'], email = "henryjknoll@gmail.com")
+    #del_a_grad(11)
+
