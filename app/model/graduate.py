8b6c1074c10ef1ee64c584c8b37bda978643fa3d
TheoKnoll <taknoll@princeton.edu>
2022-03-21 10:53:57 -0400

    Created the graduate class.

---
+++ app/model/graduate.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+
+class Graduate:
+    """
+    Graduate objects represent graduate students and their relevant
+    data fields.
+    """
+
+    def __init__(self, details, industries, experiences, interests,
+                 contact):
+        self._details = details
+        self._industries = industries
+        self._experiences = experiences
+        self._interests = interests
+        self._contact = contact
+
+    def get_grad_id(self):
+        return self._details[0]
+
+    def get_name(self):
+        return self._details[1]
+
+    def get_acad_dept(self):
+        return self._details[2]
+
+    def get_bio(self):
+        return self._details[3]
+
+    def get_undergrad_institution(self):
+        return self._details[4]
+
+    def get_masters_university(self):
+        return self._details[5]
+
+    def get_research_focus(self):
+        return self._details[6]
+
+    def get_expected_grad_date(self):
+        return self._details[7]
+
+    def get_years_worked(self):
+        return self._details[8]
+
+    def get_photo_link(self):
+        return self._details[9]
+
+    def get_website_link(self):
+        return self._details[10]
+
+    def get_industries(self):
+        return self._industries
+
+    def get_experiences(self):
+        return self._experiences
+
+    def get_interests(self):
+        return self._interests
+
+    def get_contact(self):
+        return self._contact
