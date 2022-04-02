33214db76d453b93f6d7819ef670ae5fd893ac69
TheoKnoll <taknoll@princeton.edu>
2022-04-02 17:47:26 -0400

    Debugging.

--- app/controller/pustatus/pustatus.py
+++ app/controller/pustatus/pustatus.py
@@ -34,10 +34,17 @@ def is_graduate(netid):
         req_lib.configs.USERS,
         uid=newNetID,
     )
+    supernewnetid = netid[:]
+    print(newNetID == newVar)
+    req5 = req_lib.getJSON(
+        req_lib.configs.USERS,
+        uid=supernewnetid,
+    )
 
     print(req)
     print(req2)
     print(req3)
     print(req4)
+    print(req5)
     # return req[0]['pustatus'] == 'graduate'
     return True
