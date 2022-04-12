c7b77ed50a3f8ead1766b91194f6d9430f213573
TheoKnoll <taknoll@princeton.edu>
2022-04-12 11:58:06 -0400

    Added file that gets list of universities from a search.

---
+++ app/controller/universities.py
@@ -0,0 +1,23 @@
+import requests
+
+
+def get_universities(name):
+    """
+    Returns a list of universities beginning with name.
+    :param name: The name of a university
+
+    Accesses the University Domains and Names API from Hipo
+    Accessible via Github at: https://github.com/Hipo/university-domains-list-api
+
+    :return: List of universities beginning with name
+    """
+    BASE_URL = 'http://universities.hipolabs.com/search?name='
+    search_url = BASE_URL + name
+    response = requests.request("GET", search_url).json()
+    uni_list = [university['name'] for university in response]
+    uni_list.sort()
+    return uni_list
+
+
+if __name__ == "__main__":
+    get_universities("")
