f25bcd051967299c97eef3024d95cd691ebe0380
zeligsongit <zeligson@princeton.edu>
2022-04-14 00:49:42 -0400

    Search Page Responsiveness Fixes

--- app/view/script.js
+++ app/view/script.js
@@ -1,37 +1,79 @@
-/*function openNav() {
-    document.getElementById("mySidenav").style.width = "300px";
-    document.getElementById("main").style.marginLeft = "250px";
+const heightOutput = document.querySelector('#height');
+const widthOutput = document.querySelector('#width');
+
+function reportWindowSize() {
+  if(window.innerWidth >= 780){
+    document.getElementById("mySidenav").style.width = "360px";
+    document.getElementById("mySidenav").style.height = "calc(100vh - 50px)";
+    document.getElementById("details").style.width = "calc(100vw - 425px)";
+    document.getElementById("details").style.height = "calc(100vh - 90px)";
+    document.getElementById("main").style.marginLeft = "315px";
+    document.getElementById("mySidenav").classList.add("menu-open");
+    document.getElementById("sortby").style.height="40px";
+    document.getElementById("sortby").style.marginTop="40px";
+    document.getElementById("filter").style.visibility = "visible";
   }
-  
-function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
-    document.getElementById("main").style.marginLeft= "0";
-} */
+
+  else{
+      document.getElementById("mySidenav").style.height = "525px";
+      document.getElementById("mySidenav").style.boxShadow = "none";
+      document.getElementById("mySidenav").style.width = "100vw";
+      document.getElementById("main").style.marginLeft= "0px";
+      document.getElementById("mySidenav").classList.add("menu-open");
+      document.getElementById("filter").style.visibility = "visible";
+      document.getElementById("details").style.width = "90vw";
+      document.getElementById("details").style.height = "calc(100vh - 150px)";
+      document.getElementById("sortby").style.height="40px";
+      document.getElementById("sortby").style.marginTop="40px";
+  }
+}
+
+window.onresize = reportWindowSize;
 
 function openNav() {
   if(document.getElementById("mySidenav").className.includes("menu-closed")) {
-      document.getElementById("mySidenav").style.width = "360px";
       document.getElementById("filter").style.visibility = "visible";
       document.getElementById("mySidenav").classList.add("menu-open");
       document.getElementById("mySidenav").classList.remove("menu-closed");
-      document.getElementById("main").style.marginLeft = "315px";
-      document.getElementById("sort").style.height="40px";
-      document.getElementById("sort").style.marginTop="40px";
-      document.getElementById("search").style.height="80px";
-      document.getElementById("details").style.width = "calc(100vw - 425px)";
+      document.getElementById("sortby").style.height="40px";
+      document.getElementById("sortby").style.marginTop="40px";
+
+      if (window.innerWidth > 780){
+        document.getElementById("mySidenav").style.width = "360px";
+        document.getElementById("details").style.width = "calc(100vw - 425px)";
+        document.getElementById("details").style.height = "calc(100vh - 90px)";
+        document.getElementById("main").style.marginLeft = "315px";
+        document.getElementById("mySidenav").style.height = "calc(100vh - 50px)";
+      }
+      else{
+        document.getElementById("mySidenav").style.height = "525px";
+        document.getElementById("mySidenav").style.width = "100vw";
+        document.getElementById("details").style.width = "90vw";
+        document.getElementById("details").style.height = "calc(100vh - 150px)";
+        document.getElementById("main").style.marginLeft = "0";
+        
+      }
   }
 
   else{
       document.getElementById("mySidenav").classList.remove("menu-open");
       document.getElementById("mySidenav").classList.add("menu-closed");  
       document.getElementById("filter").style.visibility = "hidden";
-      document.getElementById("mySidenav").style.width = "0";
-      document.getElementById("sort").style.height="0";
-      document.getElementById("search").style.height="0";
+      document.getElementById("sortby").style.height="0";
       document.getElementById("mySidenav").style.boxShadow = "none";
-      
-      document.getElementById("main").style.marginLeft= "20px";
-      document.getElementById("details").style.width = "calc(100vw - 110px)";
+
+      if (window.innerWidth > 780){
+        document.getElementById("mySidenav").style.width = "20px";
+        document.getElementById("details").style.width = "calc(100vw - 110px)";
+        document.getElementById("details").style.height = "calc(100vh - 90px)";
+        document.getElementById("main").style.marginLeft= "20px";
+        document.getElementById("mySidenav").style.height = "calc(100vh - 50px)";
+      }
+      else{
+        document.getElementById("mySidenav").style.height = "90px";
+        document.getElementById("details").style.height = "calc(100vh - 400px)";
+        document.getElementById("details").style.width = "90vw";
+      }      
   }    
 }
 
