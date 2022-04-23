25e4f1386ecebfee9f89a15bb8b09d0928b9c741
TheoKnoll <taknoll@princeton.edu>
2022-04-23 15:11:27 -0400

    Created departments.py, which returns a list of all graduate departments.

---
+++ app/model/departments.py
@@ -0,0 +1,26 @@
+def dept_list():
+    """
+    :return: List of all graduate departments at Princeton. Updated April 2022.
+    """
+    departments = [
+        "Anthropology", "Applied and Computational Math",
+        "Art and Archaeology", "Astrophysical Sciences",
+        "Atmospheric and Ocean Sciences",
+        "Chemical and Biological Engineering", "Chemistry",
+        "Civil and Environmental Engineering", "Classics",
+        "Comparative Literature", "Computer Science",
+        "East Asian Studies", "Ecology and Evolutionary Biology",
+        "Economics", "Electrical and Computer Engineering",
+        "English", "Finance", "French and Italian", "Geosciences",
+        "German", "History", "History of Science", "Mathematics",
+        "Mechanical and Aerospace Engineering", "Molecular Biology",
+        "Music Composition", "Musicology", "Near Eastern Studies",
+        "Neuroscience", "Operations Research and Financial Engineering",
+        "Philosophy", "Physics", "Plasma Physics", "Politics",
+        "Population Studies",
+        "Princeton School of Public and International Affairs",
+        "Psychology", "Quantitative and Computational Biology",
+        "Religion", "Slavic Languages and Literatures", "Sociology",
+        "Spanish and Portuguese"
+    ]
+    return departments
